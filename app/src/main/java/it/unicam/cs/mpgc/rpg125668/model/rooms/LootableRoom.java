228d1f68ef0602543330f86b68bb3d26c50831a8
Nicolò Di Napoli <nicolo.dinapoli@studenti.unicam.it>
2026-06-03 17:51:46 +0200

    rimozione coins

--- app/src/main/java/it/unicam/cs/mpgc/rpg125668/model/rooms/LootableRoom.java
+++ app/src/main/java/it/unicam/cs/mpgc/rpg125668/model/rooms/LootableRoom.java
@@ -4,17 +4,17 @@ import it.unicam.cs.mpgc.rpg125668.model.consumable.interfaces.IItem;
 import it.unicam.cs.mpgc.rpg125668.model.inventory.Inventory;
 import it.unicam.cs.mpgc.rpg125668.model.inventory.interfaces.IInventory;
 import it.unicam.cs.mpgc.rpg125668.model.rooms.interfaces.IRoom;
-import it.unicam.cs.mpgc.rpg125668.model.rooms.interfaces.Lootable;
+import it.unicam.cs.mpgc.rpg125668.model.rooms.interfaces.IRoomLootable;
 
 import java.util.HashMap;
 import java.util.List;
 
-public class LootableRoom extends Room implements Lootable {
+public class LootableRoom extends Room implements IRoomLootable {
     private IInventory roomInventory;
 
-    public LootableRoom(String name, List<IRoom> exits, String description, IInventory roomInventory, int coinsLotable) {
+    public LootableRoom(String name, List<IRoom> exits, String description, IInventory roomInventory) {
         super(name, exits, description);
-        if(roomInventory == null || coinsLotable < 0) throw new IllegalArgumentException("Illegal arguments: itemsLotable is null or coinsLotable < 0");
+        if(roomInventory == null) throw new IllegalArgumentException("Illegal arguments: itemsLotable is null or coinsLotable < 0");
         this.roomInventory = roomInventory;
     }
 
