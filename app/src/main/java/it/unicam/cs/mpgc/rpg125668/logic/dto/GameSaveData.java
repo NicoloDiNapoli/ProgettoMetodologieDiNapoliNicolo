a605b9d77963590294882711ec04dd79b06aa5ba
Nicolò Di Napoli <nicolo.dinapoli@studenti.unicam.it>
2026-06-12 15:27:14 +0200

    Classe che gestisce il salvataggio di una nuova partita o l'aggiornamento di una partita precedentemente salvata

---
+++ app/src/main/java/it/unicam/cs/mpgc/rpg125668/logic/dto/GameSaveData.java
@@ -0,0 +1,30 @@
+package it.unicam.cs.mpgc.rpg125668.logic.dto;
+
+import it.unicam.cs.mpgc.rpg125668.model.characters.interfaces.IStudent;
+import it.unicam.cs.mpgc.rpg125668.model.rooms.interfaces.IRoom;
+import it.unicam.cs.mpgc.rpg125668.model.skill.interfaces.IStudentSkill;
+
+import java.time.LocalDateTime;
+
+/**
+ * Class that represents the data of a game save
+ */
+public class GameSaveData {
+
+    private final String saveName;
+    private final LocalDateTime saveTime;
+    private final IStudent<IStudentSkill> player;
+    private final IRoom currentRoom;
+
+    public GameSaveData(String saveName, LocalDateTime saveTime, IStudent<IStudentSkill> player, IRoom currentRoom) {
+        this.saveName = saveName;
+        this.saveTime = saveTime;
+        this.player = player;
+        this.currentRoom = currentRoom;
+    }
+
+    public String getSaveName() { return saveName; }
+    public LocalDateTime getSaveTime() { return saveTime; }
+    public IStudent<IStudentSkill> getPlayer() { return player; }
+    public IRoom getCurrentRoom() { return currentRoom; }
+}
